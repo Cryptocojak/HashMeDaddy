cd91cb9d2fe1a9a5b5742f1ab2c375d9779bb067
shrimpfarmer <89722856+Cryptocojak@users.noreply.github.com>
2023-09-29 23:03:40 -0700

    Update HashMeDaddy.sol
    added code lol

--- HashMeDaddy.sol
+++ HashMeDaddy.sol
@@ -1,1 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract HashMeDaddy {
+  
+  address payable public owner;
+  event OwnershipTransferred(address indexed oldOwner, address indexed newOwner);
+  event FundsWithdrawn(address indexed owner, uint256 amount);
+
+  constructor() {
+  owner = payable(msg.sender);
+  }
+
+  function hashMeDaddy(string memory _string) public pure returns(bytes32) {
+     return keccak256(abi.encodePacked(_string));
+  }
+
+  function transferOwnership(address payable newOwner) public {
+    require(msg.sender == owner, "Only daddy can transfer ownership");
+    require(newOwner != address(0), "New daddy cannot be the zero address");
+    
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+  function withdrawFunds() public {
+    require(msg.sender == owner, "Only daddy can withdraw funds");
+    
+    uint256 amount = address(this).balance;
+    emit FundsWithdrawn(owner, amount);
+    
+    owner.transfer(amount);
+  }
+
+}
 
